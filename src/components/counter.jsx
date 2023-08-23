aedb67617b495954a20ce7c45c3b792cfbb06427
Qaisar Imran <qimran@securepoint360.com>
2023-08-23 16:25:47 +0500

    r4_4: passing children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,6 +13,7 @@ class Counter extends Component {
     render() {
         return (
             <div>
+                <h4> Counter # {this.props.id}</h4>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={ () => this.handleIncrement({id:1})} className='btn btn-secondary btn-sm'>Increment</button>
             </div>
