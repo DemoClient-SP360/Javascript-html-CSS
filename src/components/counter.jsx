a8d0eadfb594c571158596a07ebf3eeee7ef4596
Qaisar Imran <qimran@securepoint360.com>
2023-08-23 11:32:07 +0500

    r3_8: rendering list in component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag 1', 'tag 2', 'tag 3']
     }
     render() {
         return (
             <React.Fragment>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className='btn btn-secondary btn-sm'>Increment</button>
+                <ul>{this.state.tags.map(tag => <li key={tag} >{tag}</li>)}</ul>
             </React.Fragment>
         );
     }
